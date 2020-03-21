799de3d874c9d527ac1066451aad214188db39bd
Jonathan Vieira <jonathansouza59@gmail.com>
2020-03-21 14:21:44 -0300

    Configuring redux.

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -6,12 +6,17 @@ import PropTypes from 'prop-types';
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
+
+import store from '../store';
+
+
 export default function RouteWrapper({
     component: Component,
     isPrivate = false,
     ...rest
 }){
-    const signed = false;
+    const signed = store.getState().auth.signed;
+    // const signed = false;
 
     if (!signed && isPrivate ){
         return <Redirect to="/" />;
